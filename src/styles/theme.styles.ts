614ebd8bc8cd739a3eed13ed7556468705f7ed6a
Rajnishj <rajnishj05@gmail.com>
2022-06-18 21:58:00 +0530

    this is initial commit

---
+++ src/styles/theme.styles.ts
@@ -0,0 +1,9 @@
+import { createMuiTheme } from "@mui/material";
+import { cyan } from "@mui/material/colors"; 
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: cyan,
+        secondary:cyan
+    }
+})
